d3ba421646dd4adff5b0d1a70b894a823ab8cff3
Johannes Würbach <johannes.wuerbach@googlemail.com>
2012-01-31 11:23:22 +0100

    Fixed js dependencies in update script

--- update.sh
+++ update.sh
@@ -10,7 +10,8 @@ rm -rf ./vendor/toolkit/twitter/bootstrap/* && \
 cp -v ../bootstrap/less/*.less ./vendor/toolkit/twitter/bootstrap/ && \
 rm -rf ./vendor/assets/javascripts/twitter/bootstrap/* && \
 cp -v ../bootstrap/js/*.js ./vendor/assets/javascripts/twitter/bootstrap/
-ls ../bootstrap/js/*.js | sed s/\s/\\n/ | sed "s/.*\///" | sed s/.js// | sed "s/^/\/\/= require twitter\/bootstrap\//" > vendor/assets/javascripts/twitter/bootstrap.js && \
+ls ../bootstrap/js/*.js | sed s/\s/\\n/ | sed "s/.*\///" | sed s/.js// | sed '/popover/d' | sed "s/^/\/\/= require twitter\/bootstrap\//" > vendor/assets/javascripts/twitter/bootstrap.js && \
+echo "//= require twitter/bootstrap/bootstrap-popover" >> vendor/assets/javascripts/twitter/bootstrap.js && \
 cp -v ../bootstrap/img/* ./vendor/assets/images/ && \
 cp vendor/toolkit/twitter/bootstrap/sprites.less sprite.less && \
 cat sprite.less | sed "s/url.\.\.\/img\//image-url\(/" > vendor/toolkit/twitter/bootstrap/sprites.less && \
